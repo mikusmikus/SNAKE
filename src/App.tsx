f8c830e86c074349bf56bed56c71ffa46f7810ad
Mikus <mikusmeikulis@gmail.com>
2020-11-27 15:30:03 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,94 @@ let grid: Grid[][];
 const fillOneCell = (cell: Cell, color: CellColor) => {
   grid[cell.y][cell.x].cell = color;
 };
+
 const possibleKeypress: Direction[] = ['ArrowUp', 'ArrowRight', 'ArrowLeft', 'ArrowDown'];
+
 let head: Cell;
 let food: Cell;
-let keyPress = '';
 const tail: Cell[] = [];
+
+// type AllColorCells = {
+//   head: Cell;
+//   food: Cell;
+//   tail: Cell[];
+// };
+
+// const allColorCells:AllColorCells = {
+//   head: {x: 2, y: 0},
+//   food: {x: _.random(GRID_SIZE - 1), y: _.random(GRID_SIZE - 1)},
+//   tail: [ {x: 0, y: 0},  {x: 1, y: 0}],
+// };
+
+const directions: Direction[] = ['ArrowRight'];
+let keyPress = '';
 let score = 0;
 
-// fillOneCell(head, 'snake');
-// tail.forEach((t) => fillOneCell(t, 'tail'));
-// fillOneCell(food, 'food');
-
 const App = () => {
-  // const [head, setHead] = useState<Cell>({x: 2, y: 0});
-  // const [food, setFood] = useState<Cell>({x: _.random(9), y: _.random(9)});
-  // const [tail, setTail] = useState<Cell[]>([
-  //   {x: 0, y: 0},
-  //   {x: 1, y: 0},
-  // ]);
-  // ]);
-  // const [score, setScore] = useState(0);
-  const [moveTime, setMoveTime] = useState(200);
+  const [moveTime, setMoveTime] = useState(210);
   const [refreshGrid, setRefreshGrid] = useState(true);
   const [animation, setAnimation] = useState(false);
   const [gameOptions, setGameOptions] = useState({start: false, gameOver: false});
-  // const [keyPress, setKeyPress] = useState('');
-  // const [direction, setDirection] = useState<Direction>('ArrowRight');
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', (e) => {
+      console.log(directions);
+      keyPressHandler(e.key);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!gameOptions.start) {
+      return;
+    }
+    positionCheck();
+    moveTail();
+    moveHead(keyPress);
+    const interval = setInterval(() => {
+      setRefreshGrid(!refreshGrid);
+    }, moveTime);
+    // eslint-disable-next-line consistent-return
+    return () => clearInterval(interval);
+  }, [refreshGrid]);
+
+  // const findNextKeypress = () => {
+  //   if (directions.length > 1) {
+  //     // eslint-disable-next-line prefer-destructuring
+  //     keyPress = directions[0];
+  //     directions.splice(0, 1);
+  //     return;
+  //   }
+  //   // eslint-disable-next-line prefer-destructuring
+  //   keyPress = directions[0];
+  // };
+  const keyPressHandler = (pressedKey: string) => {
+    possibleKeypress.forEach((allowedDirection) => {
+      if (pressedKey === allowedDirection) {
+        if (pressedKey === 'ArrowLeft' && keyPress === 'ArrowRight') {
+          return;
+        }
+        if (pressedKey === 'ArrowRight' && keyPress === 'ArrowLeft') {
+          return;
+        }
+        if (pressedKey === 'ArrowDown' && keyPress === 'ArrowUp') {
+          return;
+        }
+        if (pressedKey === 'ArrowUp' && keyPress === 'ArrowDown') {
+          return;
+        }
+        // directions.push(pressedKey);
+        keyPress = pressedKey;
+      }
+    });
+  };
+
+  const positionCheck = () => {
+    tail.forEach((t) => {
+      if (t.x === head.x && t.y === head.y) {
+        setGameOptions({start: false, gameOver: true});
+      }
+    });
+  };
 
   const startGame = () => {
     setGameOptions({...gameOptions, start: true});
@@ -50,55 +112,6 @@ const App = () => {
     tail.forEach((t) => fillOneCell(t, 'tail'));
     fillOneCell(food, 'food');
     setRefreshGrid(!refreshGrid);
-  };
-
-  const keyPressHandler = (pressedKey: string) => {
-    possibleKeypress.forEach((direction) => {
-      if (pressedKey === direction) {
-        if (pressedKey === 'ArrowLeft' && keyPress === 'ArrowRight') {
-          return;
-        }
-        if (pressedKey === 'ArrowRight' && keyPress === 'ArrowLeft') {
-          return;
-        }
-        if (pressedKey === 'ArrowDown' && keyPress === 'ArrowUp') {
-          return;
-        }
-        if (pressedKey === 'ArrowUp' && keyPress === 'ArrowDown') {
-          return;
-        }
-        keyPress = pressedKey;
-      }
-    });
-  };
-
-  useEffect(() => {
-    document.body.addEventListener('keydown', (e) => {
-      keyPressHandler(e.key);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (!gameOptions.start) {
-      return;
-    }
-    positionCheck();
-
-    moveTail();
-    moveHead(keyPress);
-    const interval = setInterval(() => {
-      setRefreshGrid(!refreshGrid);
-    }, moveTime);
-    // eslint-disable-next-line consistent-return
-    return () => clearInterval(interval);
-  }, [refreshGrid]);
-
-  const positionCheck = () => {
-    tail.forEach((t) => {
-      if (t.x === head.x && t.y === head.y) {
-        setGameOptions({start: false, gameOver: true});
-      }
-    });
   };
 
   const moveHead = (dir: string) => {
@@ -120,7 +133,6 @@ const App = () => {
         }
         fillOneCell({x: head.x, y: head.y}, 'snake');
         break;
-
       case 'ArrowLeft':
         if (head.x === 0) {
           head = {x: GRID_SIZE - 1, y: head.y};
@@ -150,7 +162,9 @@ const App = () => {
   const addNewFruit = () => {
     if (head.x === food.x && head.y === food.y) {
       if (score % 5 === 0) {
-        setMoveTime(moveTime - 10);
+        if (moveTime > 20) {
+          setMoveTime(moveTime - 10);
+        }
       }
       score += 1;
       food = {x: _.random(GRID_SIZE - 1), y: _.random(GRID_SIZE - 1)};
@@ -168,10 +182,7 @@ const App = () => {
   };
 
   const showAnimation = () => {
-    if (score === 0) {
-      return;
-    }
-    if (score % 5) {
+    if (score === 0 || score % 5) {
       return;
     }
     setAnimation(true);
@@ -183,24 +194,18 @@ const App = () => {
   return (
     <div>
       <div className="container center-xs">
-        <h1 className="heading"> snake snake snake snake snake snake</h1>
-        <span className="score"> {score} points</span>
+        <h1 className="heading">
+          {' '}
+          {score > 0 && (
+            <span className="score">
+              {score}
+              {score === 1 ? ' point' : ' points'}
+            </span>
+          )}
+        </h1>
         <div className="row center-xs game-wrapper">
           <div className="col-xs-12">
             <div className="game">
-              {gameOptions.gameOver && (
-                <button
-                  type="button"
-                  className="button button--gameOver"
-                  onClick={() => {
-                    setGameOptions({start: false, gameOver: false});
-                  }}
-                >
-                  {' '}
-                  <span className="score"> Congratz, you got {score} points! </span>
-                  Play Again!
-                </button>
-              )}
               {!gameOptions.start ? (
                 <button type="button" className="button" onClick={() => startGame()}>
                   {' '}
@@ -227,6 +232,20 @@ const App = () => {
                       ))
                     )}
                 </>
+              )}
+              {gameOptions.gameOver && (
+                <button
+                  type="button"
+                  className="button button--gameOver"
+                  onClick={() => {
+                    setGameOptions({start: false, gameOver: false});
+                    score = 0;
+                  }}
+                >
+                  {' '}
+                  <span className="score"> Congratz, you got {score} points! </span>
+                  Play Again!
+                </button>
               )}
             </div>
           </div>
